7ef855e05b8e51a880da8c590c6deff72d8f3d68
Nicholas Kipkoech <nickey968@gmail.com>
2024-05-20 15:44:59 +0300

    ...

--- src/config/report-config.ts
+++ src/config/report-config.ts
@@ -35,3 +35,21 @@ export const getTaxInvoiceReportConfig = (
 
   return reportUrl;
 };
+
+export const getJournaleReportConfig = (
+  docNumber: string | number,
+  docIndex: string | number
+) => {
+  let reportUrl: string | undefined | null = "";
+  if (process.env.COMPANY === "INTRA") {
+    reportUrl = `http://192.168.1.112:9002/reports/rwservlet?userid=icon/IC0N@bima19c&module=D:/icon/forms_version/ap/reports/mayfair_ke/AP_CR_NOTE&rep_doc_no=${docNumber}&p_os_code=01&p_role_code=GL.MGR&rep_param8=&p_grp_code=GL.MGR&rep_param1=&p_module_name=AP_CR_NOTE&p_org_code=50&p_menu_code=GL000003&rep_param6=&rep_param5=&p_report_title=CLAIMS%20CREDIT%20NOTE&rep_param3=&p_user_name=ICON,%20Admin%20&rep_doc_index=28073&p_user_code=1000000&rep_param7=&destype=cache&rep_doc_org=50&rep_param2=&desformat=PDF&rep_param9=&rep_param4=&`;
+  } else if (process.env.COMPANY === "MAYFAIR") {
+    reportUrl = process.env.MAYFAIR_REPORT_URL;
+  } else if (process.env.COMPANY === "MAYFAIR_TEST") {
+    reportUrl = `http://192.168.0.210:9002/reports/rwservlet?userid=ICON/B1MA@test19c&module=F:/icon/forms_version/ar/reports/mayfair_ke/AR_RECEIPT&rep_doc_no=${docNumber}&p_os_code=01&p_role_code=AR.MGR&rep_param8=&p_grp_code=AR.MGR&rep_param1=&p_module_name=AR_RECEIPT&p_org_code=50&p_menu_code=AR000001&rep_param6=&rep_param5=&p_report_title=RECEIPT&rep_param3=&p_user_name=ICON,Admin &rep_doc_index=${docIndex}&p_user_code=1000000&rep_param7=&destype=cache&rep_doc_org=50&rep_param2=&desformat=PDF&rep_param9=&rep_param4=&`;
+  } else {
+    reportUrl = null;
+  }
+
+  return reportUrl;
+};
--- src/controllers/claimCreditNotes/claim-credit-notes-controller.ts
+++ src/controllers/claimCreditNotes/claim-credit-notes-controller.ts
@@ -1,7 +1,7 @@
 import { Response, Request } from "express";
 import pool from "../../config/oracledb-connect";
 import { config } from "dotenv";
-import { getARReceiptsReportConfig } from "../../config/report-config";
+import { getJournaleReportConfig } from "../../config/report-config";
 config();
 class ClaimCreditNotesController {
   async getClaimCreditNotes(req: Request, res: Response) {
@@ -105,7 +105,7 @@ class ClaimCreditNotesController {
           posted: row[16],
           narration: row[5],
           insured: row[25],
-          receiptUrl: getARReceiptsReportConfig(row[1], row[2]),
+          receiptUrl: getJournaleReportConfig(row[1], row[2]),
         }));
         res.json({
           success: true,
