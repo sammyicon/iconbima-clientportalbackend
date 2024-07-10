9fac1593f64bde0f297b846862a7f80011300da4
Nicholas Kipkoech <nickey968@gmail.com>
2024-07-10 11:51:13 +0300

    update report url

--- src/config/report-config.ts
+++ src/config/report-config.ts
@@ -43,7 +43,7 @@ export const getJournaleReportConfig = (
 ) => {
   let reportUrl: string | undefined | null = "";
   if (process.env.COMPANY === "INTRA") {
-    reportUrl = `http://192.168.1.112:9002/reports/rwservlet?userid=icon/IC0N@bima19c&module=D:/icon/forms_version/ap/reports/mayfair_ke/AP_CR_NOTE&rep_doc_no=${docNumber}&p_os_code=01&p_role_code=GL.MGR&rep_param8=&p_grp_code=GL.MGR&rep_param1=&p_module_name=AP_CR_NOTE&p_org_code=50&p_menu_code=GL000003&rep_param6=&rep_param5=&p_report_title=CLAIMS%20CREDIT%20NOTE&rep_param3=&p_user_name=ICON,%20Admin%20&rep_doc_index=28073&p_user_code=1000000&rep_param7=&destype=cache&rep_doc_org=50&rep_param2=&desformat=PDF&rep_param9=&rep_param4=&`;
+    reportUrl = `http://192.168.1.112:9002/reports/rwservlet?userid=icon/IC0N@bima19c&module=E:/icon/forms_version/ap/reports/mayfair_ke/AP_CR_NOTE&rep_doc_no=${docNumber}&p_os_code=01&p_role_code=GL.MGR&rep_param8=&p_grp_code=GL.MGR&rep_param1=&p_module_name=AP_CR_NOTE&p_org_code=50&p_menu_code=GL000003&rep_param6=&rep_param5=&p_report_title=CLAIMS CREDIT NOTE&rep_param3=&p_user_name=ICON,Admin &rep_doc_index=${docIndex}&p_user_code=1000000&rep_param7=&destype=cache&rep_doc_org=50&rep_param2=&desformat=PDF&rep_param9=&rep_param4=&`;
   } else if (process.env.COMPANY === "MAYFAIR") {
     reportUrl = process.env.MAYFAIR_REPORT_URL;
   } else if (process.env.COMPANY === "MAYFAIR_TEST") {
